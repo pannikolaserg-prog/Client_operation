c803e3eed15879edc8d4d779e6b42f4bb24c886a
Nikolay <pannikolaserg@gmail.com>
2025-11-08 22:52:24 +0900

    add main and delete print from widget

--- src/widget.py
+++ src/widget.py
@@ -36,15 +36,7 @@ def mask_account_card(text: str) -> str:
     return text  # если не нашли что маскировать
 
 
-# Тестирование
-print(mask_account_card("Visa Platinum 7000792289606361"))
-# Visa Platinum 7000 79** **** 6361
 
-print(mask_account_card("Счет 73654108430135874305"))
-# Счет **4305
-
-print(mask_account_card("MasterCard Gold 5500000000000004"))
-# MasterCard Gold 5500 00** **** 0004
 
 
 def get_date(date_string: str) -> str:
@@ -60,6 +52,4 @@ def get_date(date_string: str) -> str:
         return "Некорректный формат даты"
 
 
-# Тестирование
-print(get_date("2024-03-11T02:26:18.671407"))  # 11.03.2024
-print(get_date("2024-12-01T15:30:45.123456"))  # 01.12.2024
+
